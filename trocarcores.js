2b0178f93856760a464aad77d30c48546691017c
Paulo Welton <103148502+paulowelton@users.noreply.github.com>
2023-06-08 19:43:08 -0300

    Add files via upload

---
+++ trocarcores.js
@@ -0,0 +1,48 @@
+let btnrosa = document.querySelector('.rosa')
+        function rosa(){
+            document.body.style.background = 'pink'
+        }
+        let btnverde = document.querySelector('.verde')
+        function verde(){
+            document.body.style.background = 'green'
+        }
+        let btnvermelho = document.querySelector('.vermelho')
+        function vermelho(){
+            document.body.style.background = 'red'
+        }
+        let btnazul = document.querySelector('.azul')
+        function azul(){
+            document.body.style.background = 'blue'
+        }
+        let btnroxo = document.querySelector('.roxo')
+        function roxo(){
+            document.body.style.background = 'purple'
+        }
+        let btnamarelo = document.querySelector('.amarelo')
+        function amarelo(){
+            document.body.style.background = 'yellow'
+        }
+        let btnlaranja = document.querySelector('.laranja')
+        function laranja(){
+            document.body.style.background = 'orange'
+        }
+        let btnpreto = document.querySelector('.preto')
+        function preto(){
+            document.body.style.background = 'black'
+        }
+        let btncinza = document.querySelector('.cinza')
+        function cinza(){
+            document.body.style.background = 'gray'
+        }
+        let btnmarrom = document.querySelector('.marrom')
+        function marrom(){
+            document.body.style.background = 'rgb(102, 69, 6)'
+        }
+        let btnvinho = document.querySelector('.vinho')
+        function vinho(){
+            document.body.style.background = 'rgb(100, 0, 8)'
+        }
+        let btnvioleta = document.querySelector('.violeta')
+        function violeta(){
+            document.body.style.background = 'violet'
+        }
